f27f52042d343df16dffe41f3aa3d8e8ed9ad241
syzio99 <sm17dec@gmail.com>
2019-06-22 11:22:11 +0530

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ShellComponent } from './shell/shell.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { TrendingComponent } from './trending/trending.component';
+import { NewsComponent } from './news/news.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GamesComponent } from './games/games.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ShellComponent,
+    FooterComponent,
+    HomeComponent,
+    TrendingComponent,
+    NewsComponent,
+    AboutUsComponent,
+    GamesComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
