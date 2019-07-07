4250ad6e07586f40b457e01097f783b00cb3ba33
syzio99 <sm17dec@gmail.com>
2019-07-07 20:39:50 +0530

    delte method in product management is completed

--- src/app/store-page/admin/admin-products/admin-products.component.ts
+++ src/app/store-page/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductService } from './../../services/product.service';
 import { Router } from '@angular/router';
 
@@ -7,16 +7,19 @@ import { Router } from '@angular/router';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent{
 
   allProducts;
   constructor(private ProductService:ProductService) {
     this.allProducts = this.ProductService.fetchProduct();
-   
    }
-  count =0;
-   call(){return this.count +=1;}
-  ngOnInit() {
-  }
+  // count =0;
+  //  call(){return this.count +=1;}
+    delete(uid){
+      const delteProduct = confirm("Are You Sure ?");
+      if(delteProduct){
+        this.ProductService.deleteProduct(uid);
+      }
+    }
 
 }
